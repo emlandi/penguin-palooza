1eeab2fb6a1f7f27e568518262ae2877b9223794
Emily Landi <emily.landi@gmail.com>
2015-09-28 20:45:32 -0700

    added styling using jquery

---
+++ penguin-palooza.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+ alert("Caution: Penguins Ahead");
+});
+
+$("body").css("background-color", "lightblue")
+ .append("These are my penguin friends!")
+ .css("color", "white")
+ .css("font-size", 30)
+ .css("text-align", "center");
+
+$("button").css("color", "darkorange")
+ .css("font-family", "Georgia")
+ .css("margin", 5)
+ .css("padding", 20);
